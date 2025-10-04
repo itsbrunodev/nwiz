fe4dcdb7957c15a23835772eef9209f9fc250553
itsbrunodev <brunka215@gmail.com>
2025-10-04 15:59:15 +0200

    feat: add tauri

--- vite.config.ts
+++ vite.config.ts
@@ -5,15 +5,41 @@ import { defineConfig } from "vite";
 
 import packageJson from "./package.json";
 
+const host = process.env.TAURI_DEV_HOST;
+
 // https://vite.dev/config/
 export default defineConfig({
+  clearScreen: false,
+  server: {
+    port: 5173,
+    strictPort: true,
+    host: host || false,
+    hmr: host
+      ? {
+          protocol: "ws",
+          host,
+          port: 1421,
+        }
+      : undefined,
+
+    watch: {
+      ignored: ["**/src-tauri/**"],
+    },
+  },
+  envPrefix: ["VITE_", "TAURI_ENV_*"],
+  define: {
+    "import.meta.env.APP_VERSION": JSON.stringify(packageJson.version),
+  },
+  build: {
+    target:
+      process.env.TAURI_ENV_PLATFORM === "windows" ? "chrome105" : "safari13",
+    minify: !process.env.TAURI_ENV_DEBUG ? "esbuild" : false,
+    sourcemap: !!process.env.TAURI_ENV_DEBUG,
+  },
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-  },
-  define: {
-    "import.meta.env.APP_VERSION": JSON.stringify(packageJson.version),
   },
 });
