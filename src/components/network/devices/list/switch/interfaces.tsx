0303b0c38f984ed8558b119bfad6e28c5ef1e7f8
itsbrunodev <brunka215@gmail.com>
2025-10-07 23:04:05 +0200

    refactor: change calculate switchports button to small

--- src/components/network/devices/list/switch/interfaces.tsx
+++ src/components/network/devices/list/switch/interfaces.tsx
@@ -77,7 +77,11 @@ export function InterfacesTab({ switchId }: { switchId: string }) {
                 connected to this interface. Default behavior is <b>Access</b>.
               </p>
             </div>
-            <Button onClick={handleCalculateSwitchports} variant="outline">
+            <Button
+              variant="outline"
+              size="sm"
+              onClick={handleCalculateSwitchports}
+            >
               <Wand2Icon />
               Calculate Switchports
             </Button>
