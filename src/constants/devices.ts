51777de0e0e5f71da8b96d4ef3cb10bedaf66cab
itsbrunodev <brunka215@gmail.com>
2025-10-11 14:56:59 +0200

    feat: add loads of missing devices

--- src/constants/devices.ts
+++ src/constants/devices.ts
@@ -1,7 +1,27 @@
-export const ROUTERS = ["4331", "4321", "1941", "2901", "2911"] as const;
+export const ROUTERS = [
+  "ISR4331",
+  "ISR4321",
+  "1941",
+  "2901",
+  "2911",
+  "Router-PT",
+  "1841",
+  "2620XM",
+  "2621XM",
+  "2811",
+] as const;
 
 export type RouterModel = (typeof ROUTERS)[number];
 
-export const SWITCHES = ["2960"] as const;
+export const SWITCHES = [
+  "2960-24TT",
+  "Switch-PT",
+  "Switch-PT-Empty",
+  "3560-24PS",
+  "3650-24PS",
+  "IE-2000",
+  "2950-24",
+  "2950T-24",
+] as const;
 
 export type SwitchModel = (typeof SWITCHES)[number];
