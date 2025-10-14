e8ca0521cec42394b74a21ad89d8ce5fb6c1d445
itsbrunodev <brunka215@gmail.com>
2025-10-14 21:39:11 +0200

    feat: store urls in a constant

---
+++ src/constants/urls.ts
@@ -0,0 +1,9 @@
+export const URLS = {
+  github: "https://github.com/itsbrunodev/nwiz",
+  readme: "https://github.com/itsbrunodev/nwiz#readme",
+  issues: "https://github.com/itsbrunodev/nwiz/issues",
+  license: "https://github.com/itsbrunodev/nwiz/blob/main/LICENSE.md",
+  howToUse: "https://github.com/itsbrunodev/nwiz/wiki/How-to-Use",
+  importFromPacketTracer:
+    "https://github.com/itsbrunodev/nwiz/wiki/Import-from-Packet-Tracer",
+};
